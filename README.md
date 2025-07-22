51dc054fed1596cafd9dc93d7eb61a0099d7d5c0
otsuka harune <rune_ririri@icloud.com>
2025-07-22 18:46:02 +0900

    ログイン・登録画面作成

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # FleaMarket
+
+## 環境構築 🔗
+
+Docker ビルド
+
+・  git clone git@github.com:taewoo-0709/confirmation-test.git
+・  docker-compose up -d --build
+
+＊MySQL は、OS によって起動しない場合があるのでそれぞれの PC に合わせて docker-compose.yml ファイルを編集してください。
+
+Laravel 環境構築
+
+・ docker-compose exec php bash
+
+・ composer install
+
+・ .env.example ファイルから.env を作成し、環境変数を変更
+
+・ php artisan key:generate
+
+・ php artisan migrate
+
+・ php artisan db:seed
+
+## 使用技術 🔗
+
+・PHP 8.1.33
+
+・Laravel 8.83.29
+
+・MySQL 8.0.34
+
+・nginx 1.21.1
+
+## ER 図
+
+<img width="512" alt="スクリーンショット 2025-06-29 9 28 48" src="https://github.com/user-attachments/assets/a0946dd4-c704-41fd-acde-7b5b5f3b17b1" />
+
+## URL
+
+・開発環境：http://localhost/
+
+・phpMyAdmin：http://localhost:8080/
---
+++ src/resources/views/auth/login.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/login.css') }}?v={{ time() }}">
+@endsection
+
+@section('content')
+<div class="login-form">
+  <h2 class="login-form__heading">ログイン</h2>
+  <div class="login-form__inner">
+    <form class="login-form__form" action="/login" method="post">
+      @csrf
+      <div class="login-form__group">
+        <label class="login-form__label" for="email">メールアドレス</label>
+        <input class="login-form__input" type="email" name="email" id="email">
+        <p class="register-form__error-message">
+          @error('email')
+          {{ $message }}
+          @enderror
+        </p>
+      </div>
+      <div class="login-form__group">
+        <label class="login-form__label" for="password">パスワード</label>
+        <input class="login-form__input" type="password" name="password" id="password">
+        <p class="register-form__error-message">
+          @error('password')
+          {{ $message }}
+          @enderror
+        </p>
+      </div>
+      <input class="login-form__btn btn" type="submit" value="ログインする">
+      <a class="login-form__link" href="/register">会員登録はこちら</a>
+    </form>
+  </div>
+</div>
+@endsection
